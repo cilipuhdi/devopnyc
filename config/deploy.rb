76568b680253f59f41f1a48462bd1a45485daa5f
Jeremy Taylor <jeremy.taylor@vitals.com>
2015-02-07 15:16:46 -0500

    changing deploy/secrets (will need to revisit)
    Added .nvmrc or vagrant will probably break
    changed example.com to devop.nyc, and specified repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'devopnyc'
+set :repo_url, 'git@github.com:cilipuhdi/devopnyc.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
+# set :deploy_to, '/var/www/devopnyc'
 # set :scm, :git
 
 # set :format, :pretty
